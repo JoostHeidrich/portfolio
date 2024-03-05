d89648271ebd93ecb54c23c47de08925a06ceb16
joostheidrich <joost.heidrich@web.de>
2024-03-05 03:33:24 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { LandingPageComponent } from "./landing-page/landing-page.component";
+import { PortfolioComponent } from "./portfolio/portfolio.component";
+import { ContactComponent } from "./contact/contact.component";
+import { FooterComponent } from "./footer/footer.component";
+import { AboutMeComponent } from "./about-me/about-me.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [CommonModule, RouterOutlet, HeaderComponent, LandingPageComponent, PortfolioComponent, ContactComponent, FooterComponent, AboutMeComponent]
 })
 export class AppComponent {
   title = 'portfolio';
