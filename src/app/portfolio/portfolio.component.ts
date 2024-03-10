76df8efbf6a65185d7852611fa256cc8df547c30
joostheidrich <joost.heidrich@web.de>
2024-03-10 09:43:49 +0100

    animation

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import AOS from 'aos';
+import { first } from 'rxjs';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
   imports: [],
   templateUrl: './portfolio.component.html',
-  styleUrl: './portfolio.component.scss'
+  styleUrl: './portfolio.component.scss',
 })
 export class PortfolioComponent {
-
+  constructor() {}
+  ngOnInit() {
+    AOS.init({
+      duration: 2000,
+    });
+  }
 }
