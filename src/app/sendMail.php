e3b529f97ffb3dfbf3852faade57386dea08d15a
joostheidrich <joost.heidrich@web.de>
2024-04-15 23:50:29 +0200

    email change

--- src/app/sendMail.php
+++ src/app/sendMail.php
@@ -18,7 +18,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
             $name = $params->name;
             $message = $params->message;
     
-            $recipient = 'joost.heidrich@web.de';  
+            $recipient = 'mail@joostheidrich.de';  
             $subject = "Contact From <$email>";
             $message = "From:" . $name . "<br>" . $message ;
     
